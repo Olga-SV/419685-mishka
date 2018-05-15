1ba48f76723d534c0af1d2fdfded9d2303d2218c
Olga Smirnova <ss.ww@mail.ru>
2018-05-16 00:14:31 +0300

    Производит оптимизацию загрузки шрифтов

--- source/js/modal-catalog.js
+++ source/js/modal-catalog.js
@@ -1,22 +1,22 @@
 var buttonCatalog = document.querySelectorAll(".catalog__cost-button");
 var toningCatalog = document.querySelector(".modal");
-var popupCatalog = document.querySelector(".modal-block");
+var popupCatalog = document.querySelector(".modal__block");
 var closeCatalog = document.querySelector(".modal__button");
-var labelCatalog = popupCatalog.querySelectorAll("[name=size]");
+var labelCatalog = popupCatalog.querySelectorAll(".modal__label");
 var formCatalog = popupCatalog.querySelector(".modal__form");
 
 for (var i = 0; i < buttonCatalog.length; i++) {
   buttonCatalog[i].addEventListener("click", function (evt) {
     evt.preventDefault();
-    toningCatalog.classList.toggle("modal-toning");
-    popupCatalog.classList.toggle("modal-show");
+    toningCatalog.classList.toggle("modal--toning");
+    popupCatalog.classList.toggle("modal__block--show");
   });
 }
 
 closeCatalog.addEventListener("click", function (evt) {
   evt.preventDefault();
-  toningCatalog.classList.toggle("modal-toning");
-  popupCatalog.classList.toggle("modal-show");
+  toningCatalog.classList.toggle("modal--toning");
+  popupCatalog.classList.toggle("modal__block--show");
 });
 
 formCatalog.addEventListener("submit", function (evt) {
